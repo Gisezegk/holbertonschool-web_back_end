a4b6096283fd1c40d6d297c639c15a86d229764f
Gisezegk <anatanonamewanamedesuka2@gmai.com>
2024-05-08 19:16:22 +0000

    10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,4 +1,4 @@
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
@@ -6,34 +6,11 @@ class Car {
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(newBrand) {
-    this._brand = newBrand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(newMotor) {
-    this._motor = newMotor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(newColor) {
-    this._color = newColor;
+    const origin = this;
+    return Object.assign(Object.create(Object.getPrototypeOf(origin)), {
+      _brand: undefined,
+      _motor: undefined,
+      _color: undefined,
+    });
   }
 }
-  
-const myCar = new Car("Toyota", "V6", "Blue");
-const clonedCar = myCar.cloneCar();
-console.log(clonedCar); // Output: Car { _brand: 'Toyota', _motor: 'V6', _color: 'Blue' }
