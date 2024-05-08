2901d42d7e5bbfac6732daa2414f33f4943883da
Gisezegk <anatanonamewanamedesuka2@gmai.com>
2024-05-08 19:20:42 +0000

    8hbtn_class.js

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,31 +1,16 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = size;
+    this._location = location;
   }
 
-  set size(val) {
-    if (typeof val !== 'number') throw TypeError('size must by a number');
-  }
-
-  set location(val) {
-    if (typeof val !== 'string') throw TypeError('location must by a string');
-    this._location = val;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  valueOf() {
-    return this.size;
-  }
-
-  toString() {
-    return this.location;
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+    if (hint === 'number') {
+      return this._size;
+    }
+    return null;
   }
 }
