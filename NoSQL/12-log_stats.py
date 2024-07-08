f9882dfeed4944d2afae600040e66ba849cc6bad
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 23:03:57 +0000

    A

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
-""" Stats about Nginx logs in MongoDB """
-
+"""log stats from collection
+"""
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient()
-    nginx_collection = client.logs.nginx
 
-    logs_count = nginx_collection.count_documents({})
-    print(f"{logs_count} logs")
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}]
+
+
+def log_stats(mongo_collection, option=None):
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
+    print("IPs:")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    for ip in mongo_collection.aggregate(PIPE):
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
 
-    status_count = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
